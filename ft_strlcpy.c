a424c529f3cd803bf18c916b8f86c28725d79d48
Jehiel linarez <jehiel.py@gmail.com>
2024-03-25 12:49:38 +0100

    refactor functions.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	src_len;
