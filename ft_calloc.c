d3f2807fca9b8663ae4759cb979bd654dc114f36
Jehiel linarez <jehiel.py@gmail.com>
2024-03-25 09:01:45 +0100

    actuaizado calloc.

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,21 +14,19 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	size_t	total;
+	char	*mem;
 	size_t	i;
-	void	*p;
 
-	if (count == 0 || size == 0)
-	{
-		count = 1;
-		size = 1;
-	}
-	i = count * size;
-	p = malloc(i);
-	if (p == NULL)
-	{
+	total = count * size;
+	mem = malloc(total);
+	if (mem == NULL)
 		return (NULL);
+	i = 0;
+	while (i < total)
+	{
+		mem[i] = 0;
+		i++;
 	}
-	else
-		ft_bzero(p, i);
-	return (p);
+	return (mem);
 }
